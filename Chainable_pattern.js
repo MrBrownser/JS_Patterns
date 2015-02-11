d5434fbb8976b619afe8b225a93887c0d3fcf4f5
MrBrownser <castany.adria@gmail.com>
2015-02-11 12:44:47 +0100

    Initial commit

---
+++ Chainable_pattern.js
@@ -0,0 +1,11 @@
+var MyQuery = function() {
+  this.element;
+  this.query = function() {
+    // do something
+    return this;
+  }
+  this.on = function() {
+    // do something
+    return this;
+  }
+}
